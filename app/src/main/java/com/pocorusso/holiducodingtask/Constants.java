c072e4038c0fb942475113203201bbae36469e91
pocorusso <pocorusso@gmail.com>
2017-10-04 14:38:12 +0200

    removed obsoleted key

--- app/src/main/java/com/pocorusso/holiducodingtask/Constants.java
+++ app/src/main/java/com/pocorusso/holiducodingtask/Constants.java
@@ -4,7 +4,7 @@ package com.pocorusso.holiducodingtask;
 public class Constants {
     //TODO Put the API_KEY in gradle file so we can build different flavor of the app with different api KEYS
     //also to protect the API from exposure
-    protected static final String API_KEY = "AIzaSyDMHIsWXITcCbVFzinHkNaEk7XlsNnRcYE";
+    protected static final String API_KEY = "FILL_IN_THE_KEY";
     //Hard coded search query as per task description
     protected static final String API_QUERY = "https://www.googleapis.com/books/v1/volumes?q=dogs&maxResults=40&key=" + API_KEY;
 
